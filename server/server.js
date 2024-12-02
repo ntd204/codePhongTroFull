d1d7864f3edc66e262f4759a8be70e66fe7135de
Trong Dao <trongdao20402@gmail.com>
2024-12-02 16:29:02 +0700

    [#7] Tao tat ca các bang (model) bang sequelize-cli

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 require("dotenv").config();
 import cors from "cors";
 import initRouter from "./src/routes/index";
+import connectDatabase from "./src/config/connectDatabase";
 
 const app = express();
 app.use(
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 initRouter(app);
+connectDatabase();
 
 const port = process.env.PORT || 8888;
 const listener = app.listen(port, () => {
